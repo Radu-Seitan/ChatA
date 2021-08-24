0ca6f462e2b9b908144fa0a376f2892c64af77ec
Radu Seitan <raducseitan@gmail.com>
2021-08-24 15:07:19 +0300

    Removed unnecesary dependencies

--- ChatA.Domain/Enums/MembershipRoles.cs
+++ ChatA.Domain/Enums/MembershipRoles.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChatA.Domain.Enums
+﻿namespace ChatA.Domain.Enums
 {
     public enum MembershipRoles
     {
