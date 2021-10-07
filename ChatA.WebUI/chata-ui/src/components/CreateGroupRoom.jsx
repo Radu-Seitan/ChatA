d28c2df072e7d1cfcae66ccbd75da03a21c16d9a
Radu Seitan <raducseitan@gmail.com>
2021-10-07 13:56:01 +0300

    Added create group room

--- ChatA.WebUI/chata-ui/src/components/CreateGroupRoom.jsx
+++ ChatA.WebUI/chata-ui/src/components/CreateGroupRoom.jsx
@@ -1,14 +1,20 @@
 import axiosInstance from "../utils/axios";
+import { useState } from "react";
 
 const CreateGroupRoom = () => {
-  const createRoom = () => {
-    await axiosInstance;
+  const [name, setName] = useState("");
+
+  const createRoom = async () => {
+    const res = await axiosInstance.post(`api/messagerooms/group`, {
+      name: name,
+    });
   };
 
   return (
     <div className="create-group-room">
-      <form name="create-group-room-form" /*onSubmit={handleSubmit}*/>
+      <form name="create-group-room-form" onSubmit={createRoom}>
         <input
+          onChange={(e) => setName(e.target.value)}
           type="text"
           id="group-name"
           placeholder="Name the group"
