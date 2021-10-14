52a7d07a63238ad2e33967ecf8bb4edce6372fef
Radu Seitan <raducseitan@gmail.com>
2021-10-15 00:52:42 +0300

    Replaced localhost with domain

--- ChatA.WebUI/Startup.cs
+++ ChatA.WebUI/Startup.cs
@@ -78,7 +78,7 @@ namespace ChatA.WebUI
                 {
                     policy.AllowAnyHeader()
                         .AllowAnyMethod()
-                        .WithOrigins("https://localhost:5001")
+                        .WithOrigins("https://chata.azurewebsites.net/")
                         .AllowCredentials();
                 });
             });
