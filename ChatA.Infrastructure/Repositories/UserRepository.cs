b91002600f6fe65ea4444515006b697d4cbe85d5
Sonia <tuculia_sonia@yahoo.com>
2021-12-27 13:38:39 +0200

    Added change user details endpoint

--- ChatA.Infrastructure/Repositories/UserRepository.cs
+++ ChatA.Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using ChatA.Application.Common.Interfaces;
+﻿using ChatA.Application.Common.Exceptions;
+using ChatA.Application.Common.Interfaces;
 using ChatA.Domain.Entities;
 using ChatA.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,18 @@ namespace ChatA.Infrastructure.Repositories
         {
             var room = await _appDbContext.MessageRooms.FindAsync(roomId);
             return await _appDbContext.Users.Include(u => u.Memberships).Where(u => u.Memberships.Where(m => m.Room == room).Count() == 1).ToListAsync();
+        }
+
+        public async Task ChangeUserDetails(string userId, string username, string email)
+        {
+            var user = await _appDbContext.Users.FindAsync(userId);
+            if (user is null)
+            {
+                throw new NotFoundException("User cannot be found");
+            }
+            user.Email = email;
+            user.Username = username;
+            await _appDbContext.SaveChangesAsync();
         }
     }
 }
---
+++ ChatA.WebUI/Models/ChangeUserDetailsCommandModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatA.WebUI.Models
+{
+    public class ChangeUserDetailsCommandModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
